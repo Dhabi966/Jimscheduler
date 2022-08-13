1bc53ba730e0f0edcb0d4a8c38c29456f0d84662
Dhabi966 <jalewis012003@yahoo.com>
2022-08-13 16:16:04 -0600

    changes to html and css file

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,52 @@
-$(function () {
-    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+$(document).ready(function () {
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 })
 
-$("#saveBtn").click()(function () {
+$(".saveBtn").on("click", function () {
     console.log(this)
 })
-let = text(this).siblings(".hi").val();
-let = time(this).parent("id");
+var text = $(this).siblings(".description").val();
+var time = $(this).parent("id").attr("id");
 
 localStorage.setItem(time, text);
 
-$("#hour9 .hi").val(localStorage.getItem("hour9"));
-$("#hour10 .hi").val(localStorage.getItem("hour10"));
-$("#hour11 .hi").val(localStorage.getItem("hour11"));
-$("#hour12 .hi").val(localStorage.getItem("hour12"));
-$("#hour13 .hi").val(localStorage.getItem("hour13"));
-$("#hour14 .hi").val(localStorage.getItem("hour14"));
-$("#hour15 .hi").val(localStorage.getItem("hour15"));
-$("#hour16 .hi").val(localStorage.getItem("hour16"));
-$("#hour17 .hi").val(localStorage.getItem("hour17"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
 function timetracker() {
-    let currentHour = moment().hours();
+    var currentHour = moment().hour();
 
     $(".time-block").each(function () {
-        let blockTime = parseInt($(this).attr("id").split("hours"));
-        console.log(blockTime, currentHour)
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log(blockHour, currentHour)
     })
 }
 
-if (blockTime < currentHour) {
+if (blockHour < currentHour) {
     $(this).addClass("past");
     $(this).removeClass("present");
     $(this).removeClass("future");
 }
 
-else if (blockTime === currentHour) {
+else if (blockHour === currentHour) {
     $(this).addClass("present");
     $(this).removeClass("past");
     $(this).removeClass("future");
 }
 
-else (blockTime > currentHour) 
+else {
     $(this).addClass("future");
     $(this).removeClass("past");
     $(this).removeClass("present");
-
+}
 
 timetracker();
+
 
