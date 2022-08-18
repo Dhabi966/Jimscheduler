55d23ec5d7d1bb41423233b60d1b38827c228c22
Dhabi966 <jalewis012003@yahoo.com>
2022-08-17 21:28:14 -0600

    changes to css, js and html and added img

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,24 @@
 $(document).ready(function () {
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"))
 
     $(".saveBtn").on("click", function () {
         console.log(this);
 
-        var text = $(this).siblings(".description").val();
+        var text = $(this).siblings(".entries").val();
         var time = $(this).parent().attr("id");
 
         localStorage.setItem(time, text);
     })
 
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour9 .entries").val(localStorage.getItem("hour9"));
+    $("#hour10 .entries").val(localStorage.getItem("hour10"));
+    $("#hour11 .entries").val(localStorage.getItem("hour11"));
+    $("#hour12 .entries").val(localStorage.getItem("hour12"));
+    $("#hour13 .entries").val(localStorage.getItem("hour13"));
+    $("#hour14 .entries").val(localStorage.getItem("hour14"));
+    $("#hour15 .entries").val(localStorage.getItem("hour15"));
+    $("#hour16 .entries").val(localStorage.getItem("hour16"));
+    $("#hour17 .entries").val(localStorage.getItem("hour17"));
 
     function timeTracker() {
         var currentHour = moment().hour();
